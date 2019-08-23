873cad89b26a0d0a6805fc1bf235dedc1e372b50
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-23 12:21:21 +0100

    Validating the test navigation

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,9 @@
-const Index = () => <h1>Hello Sakthi!</h1>;
+import Link from "next/link";
+const Index = () => (
+  <Section>
+    <h1>Hello Sakthi!</h1>
+    <Link href="/test">Test</Link>
+  </Section>
+);
 
 export default Index;
---
+++ pages/test.js
@@ -0,0 +1,7 @@
+const Test = () => (
+  <Section>
+    <h1>Hello Test!</h1>
+  </Section>
+);
+
+export default Test;
