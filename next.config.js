3b4d080dfa6db4de70374cbfbd0f0bc152691c2c
Sakthikannan Subramanian <sakthikannan.subramanian@waitrose.co.uk>
2019-08-23 18:41:28 +0100

    Navigation issue fixes no 1

--- next.config.js
+++ next.config.js
@@ -7,7 +7,15 @@ module.exports = {
       "/test": { page: "/test" }
     };
   },
-  assetPrefix: !debug
-    ? "http://SakthikannanSubramanian.github.io/breaking_monolith/"
-    : ""
+  assetPrefix: !debug ? "/breaking_monolith/" : "",
+  webpack: (config, { dev }) => {
+    config.module.rules = config.module.rules.map(rule => {
+      if (rule.loader === "babel-loader") {
+        rule.options.cacheDirectory = false;
+      }
+      return rule;
+    });
+    // Important: return the modified config
+    return config;
+  }
 };
